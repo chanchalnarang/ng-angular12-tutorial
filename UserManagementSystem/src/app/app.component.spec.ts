e09e325e67c2de3e758933b1a8f5e37eeec3dbd1
chanchal Narang <smile.narang@gmail.com>
2021-07-05 16:50:44 +0530

    clarity framework added in angular 12 ng add @clr/angular run npm install command present on clarity site add scripts and styles manually on clarity side add clarity module and browserAnimation module in your app.module.ts

--- UserManagementSystem/src/app/app.component.spec.ts
+++ UserManagementSystem/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular12-With-clarity'`, () => {
+  it(`should have as title 'UserManagementSystem'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular12-With-clarity');
+    expect(app.title).toEqual('UserManagementSystem');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular12-With-clarity app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('UserManagementSystem app is running!');
   });
 });
